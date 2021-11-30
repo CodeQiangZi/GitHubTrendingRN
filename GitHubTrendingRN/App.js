459be052ed5267d7f13fad0f9c254ec37427bf82
CodeQiangZi <601623654@qq.com>
2021-11-30 15:27:35 +0800

    tabbar页面完成搭建，安装矢量图标库

--- GitHubTrendingRN/App.js
+++ GitHubTrendingRN/App.js
@@ -10,23 +10,58 @@ import type {Node} from 'react';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import HomePage from './js/page/HomePage';
 import FavoritePage from './js/page/FavoritePage';
 import MyPage from './js/page/MyPage';
 import PopularPage from './js/page/PopularPage';
 import TrendingPage from './js/page/TrendingPage';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import Entypo from 'react-native-vector-icons/Entypo'
 
 const Tab = createBottomTabNavigator();
 
 const App: () => Node = () => {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="FavoritePage" component={FavoritePage}/>
-                <Tab.Screen name="MyPage" component={MyPage}/>
-                <Tab.Screen name="PopularPage" component={PopularPage}/>
-                <Tab.Screen name="TrendingPage" component={TrendingPage}/>
-                <Tab.Screen name="HomePage" component={HomePage}/>
+            <Tab.Navigator screenOptions={({route}) => ({
+                tabBarIcon: ({color, size}) => {
+                    switch (route.name) {
+                        case 'PopularPage':
+                            return (
+                                <MaterialIcons name={'whatshot'} color={color} size={size}/>
+                            )
+                            break
+                        case 'TrendingPage':
+                            return (
+                                <Ionicons name={'md-trending-up'} color={color} size={size}/>
+                            )
+                            break
+                        case 'FavoritePage':
+                            return (
+                                <MaterialIcons name={'favorite'} color={color} size={size}/>
+                            )
+                            break
+                        case 'MyPage':
+                            return (
+                                <Entypo name={'user'} color={color} size={size}/>
+                            )
+                            break
+                    }
+                },
+            })}>
+                <Tab.Screen name="PopularPage" component={PopularPage} options={{
+                    title: '最热',
+                    tabBarBadge: 3,
+                }}/>
+                <Tab.Screen name="TrendingPage" component={TrendingPage} options={{
+                    title: '趋势',
+                }}/>
+                <Tab.Screen name="FavoritePage" component={FavoritePage} options={{
+                    title: '收藏',
+                }}/>
+                <Tab.Screen name="MyPage" component={MyPage} options={{
+                    title: '我的',
+                }}/>
             </Tab.Navigator>
         </NavigationContainer>
     );
