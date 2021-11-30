dcd391b5e20f87c1a359990f426189b29139f041
CodeQiangZi <601623654@qq.com>
2021-11-30 11:40:32 +0800

    添加tabbar导航控制器

--- GitHubTrendingRN/index.js
+++ GitHubTrendingRN/index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import WelcomePage from './js/page/WelcomePage'
+import WelcomePage from './js/page/WelcomePage'  // 4-4 5.18
 
-AppRegistry.registerComponent(appName, () => WelcomePage);
+AppRegistry.registerComponent(appName, () => App);
---
+++ GitHubTrendingRN/js/navigator/AppNavigators.js
@@ -0,0 +1,33 @@
+// import React, {Component} from 'react';
+// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+// import {NavigationContainer} from '@react-navigation/native'
+// import FavoritePage from './js/page/FavoritePage'
+//
+// const AppTab = createBottomTabNavigator()
+//
+// function HomeScreen() {
+//     return (
+//         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//             <Text>Home!</Text>
+//         </View>
+//     );
+// }
+//
+// function SettingsScreen() {
+//     return (
+//         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//             <Text>Settings!</Text>
+//         </View>
+//     );
+// }
+//
+// export default class AppNavigators extends Component {
+//     render() {
+//         <NavigationContainer>
+//             <AppTab.Navigator>
+//                 <AppTab.Screen name="Home" component={HomeScreen} />
+//                 <AppTab.Screen name="Settings" component={SettingsScreen} />
+//             </AppTab.Navigator>
+//         </NavigationContainer>
+//     }
+// }
